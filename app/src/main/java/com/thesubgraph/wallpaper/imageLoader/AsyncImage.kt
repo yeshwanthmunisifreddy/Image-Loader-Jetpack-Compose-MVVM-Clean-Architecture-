8ec81eecc0709dfc63c6f2c1e013977536624e4d
Yeshwanth <munship.yeshwanth@connectandheal.com>
2024-04-18 11:50:31 +0530

    Refactored Downloader class to use singleton pattern and updated AsyncImage composable to use shared Downloader instance

--- app/src/main/java/com/thesubgraph/wallpaper/imageLoader/AsyncImage.kt
+++ app/src/main/java/com/thesubgraph/wallpaper/imageLoader/AsyncImage.kt
@@ -1,12 +1,15 @@
 package com.thesubgraph.wallpaper.imageLoader
 
 import android.graphics.Bitmap
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
@@ -14,7 +17,10 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import com.thesubgraph.wallpaper.imageLoader.network.Downloader
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import kotlin.coroutines.cancellation.CancellationException
 
 sealed class ImageState {
     data object Loading : ImageState()
@@ -31,24 +37,37 @@ fun AsyncImage(
     placeHolder: @Composable () -> Unit = {},
     error: @Composable () -> Unit = {},
 ) {
-    val downloader = Downloader(LocalContext.current)
+    val context  = LocalContext.current
+    val downloader = remember { Downloader.getInstance(context) }
     var state by remember { mutableStateOf<ImageState>(ImageState.Loading) }
-
+    val scope = rememberCoroutineScope()
+    var job:Job?= null
     LaunchedEffect(url) {
-        state = try {
-            val bitmap = withContext(Dispatchers.Main.immediate) {
-                downloader.execute(url)
+         job = scope.launch {
+            state = try {
+                val bitmap = withContext(Dispatchers.Main.immediate) {
+                    downloader.execute(url)
+                }
+                if (bitmap != null) {
+                    ImageState.Success(bitmap)
+                } else {
+                    ImageState.Error("Failed to load image")
+                }
+            } catch (e: Exception) {
+                if (e is CancellationException) {
+                    Log.d("AsyncImage", "Coroutine was cancelled")
+                    return@launch
+                }
+                ImageState.Error(e.message ?: "Unknown error")
             }
-            if (bitmap != null) {
-                ImageState.Success(bitmap)
-            } else {
-                ImageState.Error("Failed to load image")
-            }
-        } catch (e: Exception) {
-            ImageState.Error(e.message ?: "Unknown error")
         }
     }
 
+    DisposableEffect(Unit) {
+        onDispose {
+            job?.cancel()
+        }
+    }
     when (val currentState = state) {
         is ImageState.Loading -> placeHolder()
         is ImageState.Success -> Image(
--- app/src/main/java/com/thesubgraph/wallpaper/imageLoader/network/Downloader.kt
+++ app/src/main/java/com/thesubgraph/wallpaper/imageLoader/network/Downloader.kt
@@ -8,39 +8,68 @@ import com.thesubgraph.wallpaper.imageLoader.cache.DiskCache
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.sync.Semaphore
+import kotlinx.coroutines.sync.withPermit
 import kotlinx.coroutines.withContext
-import java.net.HttpURLConnection
-import java.net.URL
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import okio.IOException
+import kotlin.coroutines.resumeWithException
 
-class Downloader(context: Context) {
+class Downloader private constructor(context: Context) {
+    companion object {
+        @Volatile
+        private var instance: Downloader? = null
+        fun getInstance(context: Context): Downloader {
+            return instance ?: Downloader(context).also { instance = it }
+        }
+    }
+
     private val scope = CoroutineScope(
         Job() + Dispatchers.IO
     )
+    private val semaphore = Semaphore(10) // Limit to 10 parallel downloads
     private val cache = DiskCache(context)
+    private val client = OkHttpClient()
+
     suspend fun execute(url : String): Bitmap? {
-        return withContext(scope.coroutineContext) { getData(url) }
+        return withContext(scope.coroutineContext) {
+            val bitmap = cache.get(url)
+            if (bitmap != null) {
+                Log.d("Downloader", "Image loaded from cache")
+                return@withContext bitmap
+            }else enqueue(url)
+        }
     }
 
-    private suspend fun getData(urlParam: String): Bitmap? {
-        // Try to get the image from the cache first
-        var bitmap = cache.get(urlParam)
-        if (bitmap != null) {
-            Log.d("Downloader", "Image loaded from cache")
-            return bitmap
-        }
+    private suspend fun enqueue(url: String): Bitmap? = semaphore.withPermit {
+        return withContext(scope.coroutineContext) {
+            val request = Request.Builder().url(url).build()
+            suspendCancellableCoroutine { continuation ->
+                client.newCall(request).enqueue(object : Callback {
+                    override fun onFailure(call: Call, e: IOException) {
+                        continuation.resumeWithException(e)
+                    }
 
-        // If the image is not in the cache, download it
-        val url = URL(urlParam)
-        val conn: HttpURLConnection = url.openConnection() as HttpURLConnection
-        conn.doInput = true
-        bitmap = BitmapFactory.decodeStream(conn.inputStream)
-        conn.disconnect()
-        Log.d("Downloader", "Image downloaded from network")
-        // Store the downloaded image in the cache
-        if (bitmap != null) {
-            cache.put(urlParam, bitmap)
+                    override fun onResponse(call: Call, response: Response) {
+                        response.body?.byteStream()?.let { inputStream ->
+                            BitmapFactory.decodeStream(inputStream)
+                        }?.let { bitmap ->
+                            continuation.resumeWith(Result.success(bitmap))
+                            Log.d("Downloader", "Image downloaded from network")
+                            cache.put(url, bitmap) // Store the downloaded image in the cache
+                        } ?: continuation.resumeWith(
+                            Result.failure(
+                                IOException("Failed to decode bitmap")
+                            )
+                        )
+                    }
+                })
+            }
         }
-
-        return bitmap
     }
 }
